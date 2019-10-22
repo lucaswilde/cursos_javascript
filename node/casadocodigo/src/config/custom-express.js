af07195bab2c349b0f8bc6e36dc35c169c4cb46d
Lucas Wilde <lucas.wilde@adp.com>
2019-10-22 09:47:48 -0300

    Adicionando middleware

--- node/casadocodigo/src/config/custom-express.js
+++ node/casadocodigo/src/config/custom-express.js
@@ -6,6 +6,13 @@ require('marko/express');
 // esse modulo retorna uma funcao
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser'); //middleware
+
+// chamar metodo use para pode usar o middleware
+// middleware é o mesmo que um filtro, nesse caso vai processar as chamadas sendo possivel acessar os dados atraves do comando req.body()
+app.use(bodyParser.urlencoded({
+    extended: true // transforma objetos complexos vindos das requisições em objetos JSON
+}));
 
 const rotas = require('../app/rotas/rotas.js');
 rotas(app);
