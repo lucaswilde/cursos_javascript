e6472c8a6b7bf577ebb3006acf2ef0c4535bb298
Lucas Wilde <lucas.wilde@adp.com>
2019-10-24 18:42:00 -0300

    Criado o metodo adicionar.

--- node/casadocodigo/src/app/infra/livro-dao.js
+++ node/casadocodigo/src/app/infra/livro-dao.js
@@ -16,6 +16,23 @@ class LivroDao{
         });
 
     }
+
+    adiciona(livro){
+        return new Promise((resolve, reject) => {
+            // run é um metodo do sqlite que executa queries sem retornar um resultado
+            this._db.run(
+                    'insert into livros (titulo, preco, descricao) values (?,?,?)'
+                    , [livro.titulo, livro.preco, livro.descricao]
+                    , function(err){
+                        if(err){
+                            console.log(err);
+                            return reject('Não foi possivel adicionar o livro');
+                        }
+                        resolve();
+                    }
+                )
+        });
+    }
 }
 
 module.exports = LivroDao;
--- node/casadocodigo/src/app/rotas/rotas.js
+++ node/casadocodigo/src/app/rotas/rotas.js
@@ -37,5 +37,9 @@ module.exports = (app) => {
 
     app.post('/livros', function(req, resp){
         console.log(req.body);
+        const livroDao = new LivroDao(db);
+        livroDao.adiciona(req.body)
+                .then(resp.redirect('/livros'))
+                .catch(erro => console.log(erro));
     });
 };
