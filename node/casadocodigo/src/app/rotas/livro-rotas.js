f3aef706fed3e75e1c0f3823fa723942638b8dbe
Lucas Wilde <lucas.wilde@adp.com>
2019-11-08 07:27:14 -0300

    Adicionado verificação de autenticação, ou seja, só acessa /livros se estiver logado.

--- node/casadocodigo/src/app/rotas/livro-rotas.js
+++ node/casadocodigo/src/app/rotas/livro-rotas.js
@@ -1,9 +1,19 @@
 const LivroControlador = require('../controladores/livro-controlador');
 const livroControlador = new LivroControlador();
 const Livro = require('../modelos/livro');
+const BaseControlador = require('../controladores/base-controlador');
 
 module.exports = (app) => {
     
+    app.use(LivroControlador.rotas().autenticadas, function(req, resp, next){
+        // isAuthenticated é injetado pelo classe passport
+        if(req.isAuthenticated()){
+            next();
+        } else{
+            resp.redirect(BaseControlador.rotas().login);
+        }
+    });
+
     app.get(LivroControlador.rotas().lista, livroControlador.lista());
 
     app.route(LivroControlador.rotas().cadastro)
