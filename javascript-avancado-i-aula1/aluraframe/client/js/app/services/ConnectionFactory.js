686b3cbbdcded4bf6f68a8830c6564fdf78331e7
Lucas Wilde <lucas.wilde@adp.com>
2019-11-24 10:41:01 -0300

    O padrão de projeto Module Pattern

--- javascript-avancado-i-aula1/aluraframe/client/js/app/services/ConnectionFactory.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/services/ConnectionFactory.js
@@ -1,38 +1,50 @@
-// declarando variaveis globais
-var stores = ['negociacoes'];
-var version = 4;
-var dbName = 'aluraframe';
+// O padrão de projeto Module Pattern
+var ConnectionFactory = (function () {
 
-class ConnectionFactory{
-    constructor(){
-        throw new Error('Não é possivel criar instâncias de ConnectionFactory');
-    }
-
-    static getConnection(){
-        return new Promise((resolve, reject) => {
-            let openRequest = window.indexedDB.open(dbName, version);
-
-            openRequest.onupgradeneeded = e =>{
-                ConnectionFactory._createStores(e.target.result);
-            };
-            
-            openRequest.onsuccess = e => {
-                resolve(e.target.result);
-            };
-            
-            openRequest.onerror = e => {
-                reject(e.target.error.name);
-            }
-        });
-    }
+    var stores = ['negociacoes'];
+    var version = 4;
+    var dbName = 'aluraframe';
+    var connection = null;
     
-    static _createStores(connection){
-        stores.forEach(store => {
-            if(connection.objectStoreNames.contains(store)) {
-                connection.deleteObjectStore(store);
-            }
+    return class ConnectionFactory{
+        constructor(){
+            throw new Error('Não é possivel criar instâncias de ConnectionFactory');
+        }
     
-            connection.createObjectStore(store, {autoIncrement: true});
-        });
+        static getConnection(){
+            return new Promise((resolve, reject) => {
+                let openRequest = window.indexedDB.open(dbName, version);
+    
+                openRequest.onupgradeneeded = e =>{
+                    console.log('getConnection, onupgradeneeded');
+                    ConnectionFactory._createStores(e.target.result);
+                };
+                
+                openRequest.onsuccess = e => {
+                    console.log('getConnection, onsuccess: connection=' + connection);
+    
+                    // se null, cria pega uma nova conexão
+                    if(!connection){ 
+                        connection = e.target.result;
+                    }
+                    resolve(e.target.result);
+                };
+                
+                openRequest.onerror = e => {
+                    console.log('getConnection, onerror');
+                    reject(e.target.error.name);
+                }
+            });
+        }
+        
+        static _createStores(connection){
+            stores.forEach(store => {
+                if(connection.objectStoreNames.contains(store)) {
+                    connection.deleteObjectStore(store);
+                }
+        
+                connection.createObjectStore(store, {autoIncrement: true});
+            });
+        }
     }
-}
+}) ();
