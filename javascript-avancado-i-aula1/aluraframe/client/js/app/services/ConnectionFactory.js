b1b52e4c1d773dbf5b71e616fabed9ea2c42b34d
Lucas Wilde <lucas.wilde@adp.com>
2019-11-24 09:52:48 -0300

    Criando stores

---
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/services/ConnectionFactory.js
@@ -0,0 +1,38 @@
+// declarando variaveis globais
+var stores = ['negociacoes'];
+var version = 4;
+var dbName = 'aluraframe';
+
+class ConnectionFactory{
+    constructor(){
+        throw new Error('Não é possivel criar instâncias de ConnectionFactory');
+    }
+
+    static getConnection(){
+        return new Promise((resolve, reject) => {
+            let openRequest = window.indexedDB.open(dbName, version);
+
+            openRequest.onupgradeneeded = e =>{
+                ConnectionFactory._createStores(e.target.result);
+            };
+            
+            openRequest.onsuccess = e => {
+                resolve(e.target.result);
+            };
+            
+            openRequest.onerror = e => {
+                reject(e.target.error.name);
+            }
+        });
+    }
+    
+    static _createStores(connection){
+        stores.forEach(store => {
+            if(connection.objectStoreNames.contains(store)) {
+                connection.deleteObjectStore(store);
+            }
+    
+            connection.createObjectStore(store, {autoIncrement: true});
+        });
+    }
+}
