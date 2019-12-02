c9694c805e1e082190ffcc6af36747d20b9de13b
Lucas Wilde <lucas.wilde@adp.com>
2019-12-01 21:37:34 -0300

    Encapsulando acesso ao metodo close

--- javascript-avancado-i-aula1/aluraframe/client/js/app/services/ConnectionFactory.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/services/ConnectionFactory.js
@@ -1,10 +1,11 @@
 // O padrão de projeto Module Pattern
 var ConnectionFactory = (function () {
 
-    var stores = ['negociacoes'];
-    var version = 4;
-    var dbName = 'aluraframe';
+    const stores = ['negociacoes'];
+    const version = 4;
+    const dbName = 'aluraframe';
     var connection = null;
+    var close = null;
     
     return class ConnectionFactory{
         constructor(){
@@ -26,8 +27,12 @@ var ConnectionFactory = (function () {
                     // se null, cria pega uma nova conexão
                     if(!connection){ 
                         connection = e.target.result;
+                        close = connection.close.bind(connection);
+                        connection.close = function() {
+                            throw new Error('Você não pode fechar diretamente a conexão');
+                        };
                     }
-                    resolve(e.target.result);
+                    resolve(connection);
                 };
                 
                 openRequest.onerror = e => {
@@ -45,6 +50,13 @@ var ConnectionFactory = (function () {
         
                 connection.createObjectStore(store, {autoIncrement: true});
             });
+        }
+
+        static closeConnection(){
+            if(connection){
+                close();
+                connection = null;
+            }
         }
     }
 }) ();
