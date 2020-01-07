fa16d854393576c9a5c89ff92119b4ddd676e8ac
lucaswilde <Wld0000>
2020-01-06 22:04:48 -0300

    Passando função como propriedade.

--- react/react-complete-guide/src/App.js
+++ react/react-complete-guide/src/App.js
@@ -13,12 +13,12 @@ class App extends Component{
     otherState: "some other value"
   };
 
-  switchStateHandler = () => {
+  switchStateHandler = (newName) => {
     // DON'T DO THIS: this.state.persons[1].name = "Fabiane";
     this.setState({
       persons: [
         {name: "Lucas", age: "30"},
-        {name: "Fabine", age: "40"},
+        {name: newName, age: "40"},
         {name: "Pirulito", age: "25"},
       ]
     });
@@ -28,10 +28,14 @@ class App extends Component{
     return(
         <div className="App">
           <h1>Hi, I'm a React App</h1>
-          <button onClick={this.switchStateHandler}>Switch Name</button>
-          <Person name={this.state.persons[0].name} age={this.state.persons[0].age}></Person>
-          <Person name={this.state.persons[1].name} age={this.state.persons[1].age}></Person>
-          <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>My hobbies: Racing</Person>
+          <button onClick={() => this.switchStateHandler('Fabine Turela')}>Switch Name</button>
+          <Person name={this.state.persons[0].name} 
+              age={this.state.persons[0].age}></Person>
+          <Person name={this.state.persons[1].name} 
+              age={this.state.persons[1].age}></Person>
+          <Person name={this.state.persons[2].name} 
+              age={this.state.persons[2].age}
+              click={this.switchStateHandler.bind(this, 'Fabiiiiica!!!')}>My hobbies: Racing</Person>
         </div>
       )
   }
--- react/react-complete-guide/src/Person/Person.js
+++ react/react-complete-guide/src/Person/Person.js
@@ -4,7 +4,7 @@ const person = (props) => {
 return (
     <div>
         <p>I'm {props.name}, I'm {props.age} years old.</p>
-        <p>{props.children}</p>
+        <p onClick={props.click}>{props.children}</p>
     </div>
     )
 };
