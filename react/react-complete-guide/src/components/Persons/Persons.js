0b550acd4fb16835bb5789b274bac20c34600f50
lucaswilde <Wld0000>
2020-01-13 15:24:33 -0300

    Using shouldComponentUpdate for Optimization

--- react/react-complete-guide/src/components/Persons/Persons.js
+++ react/react-complete-guide/src/components/Persons/Persons.js
@@ -1,23 +1,28 @@
 import React, { Component } from 'react';
 import Person from './Person/Person';
 
-class Persons extends Component{
+class Persons extends Component {
 
-    componentWillUnmount(){
+    componentWillUnmount() {
         console.log('[Persons.js] componentWillUnmount');
     }
 
-    render(){
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('[Persons.js] shouldComponentUpdate');
+        return (nextProps.persons !== this.props.persons);
+    }
+
+    render() {
         console.log('[Persons.js] redering...');
         return this.props.persons.map((person, index) => {
-            return <Person 
-          click={() => this.props.clicked(index)}
-          name={person.name} 
-          age={person.age}
-          changed={(event) => this.props.changed(event, person.id)}
-          key={person.id}
-          />
-      });
+            return <Person
+                click={() => this.props.clicked(index)}
+                name={person.name}
+                age={person.age}
+                changed={(event) => this.props.changed(event, person.id)}
+                key={person.id}
+            />
+        });
     }
 };
 
