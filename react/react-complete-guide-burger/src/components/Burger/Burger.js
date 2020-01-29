200a36bbaaba1a54e262c902666d09e8e9a2b452
lucaswilde <lucaswilde89@gmail.com>
2020-01-28 22:01:03 -0300

    Calculating the ingredients

--- react/react-complete-guide-burger/src/components/Burger/Burger.js
+++ react/react-complete-guide-burger/src/components/Burger/Burger.js
@@ -4,12 +4,20 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const Burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
                 return <BurgerIngredient key={igKey + 1} type={igKey} />
             });
-        });
+        })
+        .reduce((arr, el) => {
+                return arr.concat(el)
+            }, []
+        );
+
+    if(transformedIngredients.length === 0 ){
+        transformedIngredients = "Please start adding ingredients!";
+    }
 
     return (
         <div className={classes.Burger}>
