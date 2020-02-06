382e56329fd4ce5cb52658bc38817e5e5ec3eec2
lucaswilde <lucaswilde89@gmail.com>
2020-02-06 19:14:52 -0300

    Connecting state to building controls

--- react/react-complete-guide-burger/src/containers/BurgerBuilder/BurgerBuilder.js
+++ react/react-complete-guide-burger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,25 +1,54 @@
 import React, { Component } from "react";
 import Aux from "../../hoc/Aux";
 import Burger from "../../components/Burger/Burger.js";
+import BuildControls from "../../components/Burger/BuildControls/BuildControls";
+
+// constante global deve ser em maiusculo
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+};
 
 class BurgerBuilder extends Component{
 
     state = {
         ingredients: {
-            salad: 1,
+            salad: 0,
             bacon: 0,
             cheese: 0,
             meat: 0
-        }
+        },
+        totalPrice: 4
     }
 
     render(){
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients}/>
-                <div>Burger Controls</div>
+                <BuildControls 
+                    ingredientAdded={this.addIngredientHandler}
+                    />
             </Aux>
         );
+    }
+
+    addIngredientHandler = (type) => {
+        console.log("addIngredientHandler");
+
+        let oldCount = this.state.ingredients[type];
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = ++oldCount;
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        this.setState({totalPrice: oldPrice + priceAddition, ingredients: updatedIngredients});
+    }
+
+    removeIngredientHandler = (type) => {
+
     }
 }
 
