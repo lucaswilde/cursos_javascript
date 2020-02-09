a7c3ca114a795c280d90d7f014bcebe0419f2bc4
lucaswilde <lucaswilde89@gmail.com>
2020-02-09 11:21:55 -0300

    Creating the order summary modal

--- react/react-complete-guide-burger/src/containers/BurgerBuilder/BurgerBuilder.js
+++ react/react-complete-guide-burger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import Aux from "../../hoc/Aux";
 import Burger from "../../components/Burger/Burger.js";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
+import Modal from "../../components/UI/Modal/Modal";
+import OrderSummary from "../../components/Burger/OrderSummary/OrderSummury";
 
 // constante global deve ser em maiusculo
 const INGREDIENT_PRICES = {
@@ -46,6 +48,9 @@ class BurgerBuilder extends Component{
 
         return (
             <Aux>
+                <Modal>
+                    <OrderSummary ingredients={this.state.ingredients}/>
+                </Modal>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls 
                     ingredientAdded={this.addIngredientHandler}
