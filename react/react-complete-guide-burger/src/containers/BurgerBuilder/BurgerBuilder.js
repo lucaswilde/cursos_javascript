14f70e60ffc06dcf9969189354697565a3251eae
lucaswilde <lucaswilde89@gmail.com>
2020-02-12 15:50:14 -0300

    Showing and hiding the modal

--- react/react-complete-guide-burger/src/containers/BurgerBuilder/BurgerBuilder.js
+++ react/react-complete-guide-burger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -23,7 +23,8 @@ class BurgerBuilder extends Component{
             meat: 0
         },
         totalPrice: 4,
-        purchasable: false
+        purchasable: false,
+        purchasing: false
     }
 
     updatePurchasable(ingredients){
@@ -48,7 +49,7 @@ class BurgerBuilder extends Component{
 
         return (
             <Aux>
-                <Modal>
+                <Modal show={this.state.purchasing}>
                     <OrderSummary ingredients={this.state.ingredients}/>
                 </Modal>
                 <Burger ingredients={this.state.ingredients}/>
@@ -58,6 +59,7 @@ class BurgerBuilder extends Component{
                     disabled={disabledInfo}
                     price={this.state.totalPrice}
                     purchasable={this.state.purchasable}
+                    ordered={this.purchaseHandler}
                     />
             </Aux>
         );
@@ -93,6 +95,11 @@ class BurgerBuilder extends Component{
         this.setState({totalPrice: oldPrice - priceDeduction, ingredients: updatedIngredients});
         this.updatePurchasable(updatedIngredients);
     }
+
+    purchaseHandler = () => {
+        this.setState({purchasing: true});
+    }
+    
 }
 
 export default BurgerBuilder;
