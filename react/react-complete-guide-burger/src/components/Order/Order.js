415d4e5f70429b9a9a71ad6a5b34d32a340a85e3
lucaswilde <lucaswilde89@gmail.com>
2020-03-22 19:27:59 -0300

    Outputting the orders.

--- react/react-complete-guide-burger/src/components/Order/Order.js
+++ react/react-complete-guide-burger/src/components/Order/Order.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import classes from './Order.module.css';
 
-const Order = (props) => (
-    <div className={classes.Order}>
-        <p>Ingredients</p>
-        <p>Price</p>
-    </div>
-);
+const Order = (props) => {
+    const ingredients = [];
+    for(let ingredientName in props.ingredients){
+        ingredients.push({
+                name: ingredientName,
+                amount: props.ingredients[ingredientName]
+        });
+    }
+
+    const ingredientOutput = ingredients.map(ig => {
+        return <span style={{ 
+                textTransform: "capitalize", 
+                display: 'inline-box',
+                margin: '0 8px',
+                border: '1px solid #ccc',
+                padding: '5px'
+            }}>
+            {ig.name} ({ig.amount})</span>;
+    });
+
+    return (
+        <div className={classes.Order}>
+            <p>Ingredients: {ingredientOutput}</p>
+            <p>Price: <strong>{Number.parseFloat(props.price).toFixed(2)}</strong></p>
+        </div>
+    );
+};
 
 export default Order;
