393f762d5a58248a22c80a8c59c187fd9d23821b
lucaswilde <lucaswilde89@gmail.com>
2020-03-22 18:37:54 -0300

    Adding orders pages

---
+++ react/react-complete-guide-burger/src/components/Order/Order.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import classes from './Order.module.css';
+
+const Order = (props) => (
+    <div className={classes.Order}>
+        <p>Ingredients</p>
+        <p>Price</p>
+    </div>
+);
+
+export default Order;
